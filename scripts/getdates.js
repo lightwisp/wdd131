1624901006a4024a6c6fce5bd9a3f493a8c9a779
lightwisp <sayerdt@byuo.edu>
2025-01-09 17:09:15 -0700

    i finished my home page added java script and did alot of css

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.getElementById("year").innerHTML = year
+document.getElementById("lastModified").innerHTML = lastModified
