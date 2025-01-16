fad4bedf2a88e0cd85758db2ec2b006afc53d954
lightwisp <sayerdt@byuo.edu>
2025-01-16 08:02:04 -0700

    finishing up my learning activity

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -1,18 +1,30 @@
 const input = document.querySelector("#favchap")
-const button =document.querySelector("botton")
+const button =document.querySelector("button")
 const list = document.querySelector("ul")
-const li = document.createElement("li")
-const deleteButton = document.createElement("button")
 
-li.textContent = input.value;
-button.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
 
-function copyInput(){
+function caddChapter(){
     const inputElement = document.getElementById(input)
     const outputElement = document.getElementById(li)
     outputElement.textContent = inputElement.value
 }
 
-button.addEventListener("click", copyInput)
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const deleteButton = document.createElement("button")
+        const li = document.createElement("li")
+
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        
+        li.append(deleteButton); 
+        list.append(li); 
+
+        deleteButton.addEventListener('click', function(){
+        list.removeChild(li);  
+        });
+
+        input.value = '';
+    }
+    input.focus();
+});
