8e180adf3bde5849bd723388ba89775a10a647b6
lightwisp <sayerdt@byuo.edu>
2025-01-16 08:29:19 -0700

    testing the debugging

---
+++ week02/sandbox/debugging.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+const header = document.querySelector("h1")
+let area = 0;
+
+function circleArea(radius){
+   const area = radius * radius * PI;
+   return area
+}
+
+area = circleArea(3)
+console.log(area)
+
+area = circleArea(4)
+console.log(area)
+
+header.append(area)
