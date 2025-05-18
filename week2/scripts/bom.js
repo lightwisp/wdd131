c4948468f5073fcb472e504ae931c091385e73ef
lightwisp <sayerdt@gmail.com>
2025-05-18 15:15:33 -0600

    added event listeners to the add chapter and deleat buttons.

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -1,14 +1,31 @@
 
 const input = document.querySelector('#favchap');
-const botton = document.querySelector('botton');
-const list = document.querySelector('list')
-const li =document.createElement('li');
-const deleateButton = document.createElement('button');
+const button = document.querySelector('button');
+const list = document.querySelector('ul')
 
-li.textContent = input.value;
 
-deleateButton.textContent = '❌';
 
-li.append(deleateButton);
+button.addEventListener('click', function(){
+    if (input.value.trim() !== ''){
+        const li = document.createElement('li');
+        const deleateButton = document.createElement('button');
 
-list.append(li);
+
+        li.textContent = input.value;
+        deleateButton.textContent = '❌';
+
+        li.append(deleateButton);
+        list.append(li);
+
+        deleateButton.addEventListener('click', function(){
+            list.removeChild(li);
+            input.value = '';
+            input.focus();
+        });
+        
+    } else {
+        input.placeholder = 'Type in your favoret chapter'
+    }
+    input.focus();
+});
+
